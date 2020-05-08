6632ad963171c2080639613609b037dbf6bc24ca
Lukas Lamm <lukas.lamm89@gmail.com>
2020-05-08 15:08:47 +0200

    Initial commit

---
+++ OvertimeBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KimaiPlugin\OvertimeBundle;
+
+use App\Plugin\PluginInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OvertimeBundle extends Bundle implements PluginInterface
+{
+}
